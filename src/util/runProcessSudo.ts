071f7cc03c7af9d7b117edc530054673a9caef72
h8m0ss <danarmenta32@gmail.com>
2024-02-02 12:55:07 -0600

    DEBUG: Added some logging

--- src/util/runProcessSudo.ts
+++ src/util/runProcessSudo.ts
@@ -7,14 +7,22 @@ const runProcessSudo = async (command: string[]) => {
   await killProcess();
   const childProcess = spawn('sudo', command);
 
+  childProcess.stdout.setEncoding('utf-8');
   childProcess.stdout.on('data', (data) => {
     childProcess.stdin.write(process.env.PASSWORD + '\r\n');
     childProcess.stdin.end();
   });
+  childProcess.on('disconnect', () =>
+    console.log({ command, event: 'disconnect' })
+  );
+  childProcess.on('close', () => console.log({ command, event: 'close' }));
+  childProcess.on('error', () => console.log({ command, event: 'error' }));
+  childProcess.on('exit', () => console.log({ command, event: 'exit' }));
 
   const pid = childProcess.pid;
+  console.log({ command, pid });
+
   if (pid === undefined) throw `Error running command ${command}`;
-  childProcess.disconnect();
   await writeFile(processLocation, pid.toString());
 };
 
