075494427a243ceb0682af0e133a1896ad875e1f
h8m0ss <danarmenta32@gmail.com>
2024-02-15 12:52:08 -0600

    Testing run process sudo

--- src/util/runProcessSudo.ts
+++ src/util/runProcessSudo.ts
@@ -1,29 +1,37 @@
 import { spawn } from 'child_process';
-import { writeFile } from 'fs/promises';
-import { processLocation } from '../constants';
 import killProcess from './killProcess';
 
 const runProcessSudo = async (command: string[]) => {
   await killProcess();
   const childProcess = spawn('sudo', command);
 
-  childProcess.stdout.setEncoding('utf-8');
+  childProcess.stdout.setEncoding('utf8');
   childProcess.stdout.on('data', (data) => {
-    childProcess.stdin.write(process.env.PASSWORD + '\r\n');
-    childProcess.stdin.end();
+    console.log('STDOUT: ' + data);
   });
-  childProcess.on('disconnect', () =>
-    console.log({ command, event: 'disconnect' })
-  );
-  childProcess.on('close', () => console.log({ command, event: 'close' }));
-  childProcess.on('error', () => console.log({ command, event: 'error' }));
-  childProcess.on('exit', () => console.log({ command, event: 'exit' }));
+  childProcess.stderr.setEncoding('utf8');
+  childProcess.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+  });
 
-  const pid = childProcess.pid;
-  console.log({ command, pid });
+  childProcess.on('close', function (code) {
+    //Here you can get the exit code of the script
 
-  if (pid === undefined) throw `Error running command ${command}`;
-  await writeFile(processLocation, pid.toString());
+    console.log('closing code: ' + code);
+  });
+
+  // childProcess.on('disconnect', () =>
+  // console.log({ command, event: 'disconnect' })
+  // );
+  // childProcess.on('close', () => console.log({ command, event: 'close' }));
+  // childProcess.on('error', () => console.log({ command, event: 'error' }));
+  // childProcess.on('exit', () => console.log({ command, event: 'exit' }));
+
+  // const pid = childProcess.pid;
+  // console.log({ command, pid });
+
+  // if (pid === undefined) throw `Error running command ${command}`;
+  // await writeFile(processLocation, pid.toString());
 };
 
 export default runProcessSudo;
