7328d93531703634190944c2d0e060593316700f
h8moss <danarmenta32@gmail.com>
2024-08-15 13:01:16 -0600

    FIX: Testing tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="flex">
-      <nav className="flex flex-col">
+      <nav className="flex flex-col navbar">
         <a>Home</a>
         <a>Colors</a>
         <a>Game of life</a>
@@ -11,16 +11,16 @@ const App = () => {
         <a>Images</a>
         <a>Custom</a>
         <a>Files</a>
-        <div className="spacer" />
+        <div className="flex-1" />
         <a>Configuration</a>
       </nav>
       <div>
         <h1>Welcome to the led matrix server!</h1>
         <h2>Quick actions</h2>
-        <div className="quick-actions">
-          <a>Colors</a >
-          <a>Game of life</a >
-          <a>Time date</a >
+        <div>
+          <a>Colors</a>
+          <a>Game of life</a>
+          <a>Time date</a>
           <a>Images</a>
         </div>
       </div>
