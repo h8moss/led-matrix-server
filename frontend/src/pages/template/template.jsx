16cc5d7582d86a0a01a68956ce7f9373f2dac591
h8m0ss <danarmenta32@gmail.com>
2024-08-22 10:22:32 -0600

    STYLE: Fixed overflow on template

--- frontend/src/pages/template/template.jsx
+++ frontend/src/pages/template/template.jsx
@@ -5,7 +5,7 @@ const Template = () => {
   return (
     <div className="flex h-screen w-screen overflow-x-hidden">
       <Navbar />
-      <div className="flex-1 flex p-2 flex-col">
+      <div className="flex-1 flex p-2 flex-col overflow-y-auto">
         <Outlet />
       </div>
     </div>
